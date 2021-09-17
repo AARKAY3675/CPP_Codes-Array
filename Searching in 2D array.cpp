2f04b602da322d39792d85d5c5877a9953a4d814
Rithik Sharma <76279201+AARKAY3675@users.noreply.github.com>
2021-09-18 00:52:27 +0530

    C++ Codes

---
+++ Searching in 2D array.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int x,y,i,j;
+   cin>>x>>y;
+   int arr[x][y];
+   
+   for(i=0;i<x;i++)
+   {
+       for(j=0;j<y;j++)
+       {
+           cin>>arr[i][j];
+       }
+   }
+   cout<<"array entered is \n";
+   for(i=0;i<x;i++)
+   {
+       for(j=0;j<y;j++)
+       {
+           cout<<arr[i][j]<<" ";
+       }
+    cout<<"\n";
+   }
+   int a;
+   cout<<"Enter the element to be searched";
+   cin>>a;
+   for(i=0;i<x;i++)
+   {
+       for(j=0;j<y;j++)
+       {
+ if( arr[i][j]==a)
+          cout<<"Element found";
+          }
+   }
+    return 0;
+}
+
